6d7d4beb28735466acb99c37e09a1d6302fdd67c
zachmanac <zachstephenson94@gmail.com>
2023-10-02 16:26:35 -0700

    fix typo

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
